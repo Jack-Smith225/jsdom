0982cbc88cf1d515508d236e677f05dc0c4cade7
zy <1475705241@qq.com>
2021-04-01 15:56:40 +0800

    5秒之后移动元素的位置

--- chp10/pos.js
+++ chp10/pos.js
@@ -10,12 +10,25 @@ function positionMessage() {
   elem.style.position = "absolute"
   elem.style.left = "50px";
   elem.style.top = "100px";
+  movement = setTimeout("moveMessage()", 5000);
+}
+
+function moveMessage() {
+  if (!document.getElementById) {
+    return false;
+  }
+  if (!document.getElementById("message")) {
+    return false;
+  }
+  var elem = document.getElementById("message");
+  elem.style.left = "200px";
 }
 
 //window.onload = positionMessage;
 
 function addLoadEvent(func) {
   var oldOnload = window.onload;
+  //若之前的onload没被赋予函数,则直接调用; 否则首先调用老的
   if (typeof window.onload != 'function') {
     window.onload = func;
   } else {
