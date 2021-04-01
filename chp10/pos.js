17affdbf721be9e094c2e5af6e577473dec60693
zy <1475705241@qq.com>
2021-04-01 16:10:47 +0800

    实现动画

--- chp10/pos.js
+++ chp10/pos.js
@@ -10,10 +10,10 @@ function positionMessage() {
   elem.style.position = "absolute"
   elem.style.left = "50px";
   elem.style.top = "100px";
-  movement = setTimeout("moveMessage()", 5000);
+  movement = setTimeout("moveMessage()", 2000);
 }
 
-function moveMessage() {
+function  moveMessage() {
   if (!document.getElementById) {
     return false;
   }
@@ -21,7 +21,27 @@ function moveMessage() {
     return false;
   }
   var elem = document.getElementById("message");
-  elem.style.left = "200px";
+  var xpos = parseInt(elem.style.left);
+  var ypos = parseInt(elem.style.top);
+  if (xpos == 200 && ypos == 100) {
+    return true;
+  }
+  if (xpos < 200) {
+    xpos+=1;
+  }
+  if (xpos > 200) {
+    xpos-=1;
+  }
+  if (ypos < 200) {
+    ypos+=1;
+  }
+  if (ypos > 200) {
+    ypos-=1;
+  }
+
+  elem.style.left = xpos + "px";
+  elem.style.top = ypos + "px";
+  movement = setTimeout("moveMessage()", 10);
 }
 
 //window.onload = positionMessage;
