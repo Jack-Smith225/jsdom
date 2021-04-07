91a743ed941b33ac47fd792bf5e855d30aafd601
zy <1475705241@qq.com>
2021-04-07 16:12:35 +0800

    10.2.4-切换长图片显示区域

---
+++ chp10/10-2/scripts/addLoadevent.js
@@ -0,0 +1,19 @@
+
+
+function addLoadEvent(func) {
+  var oldOnload = window.onload;
+  //若之前的onload没被赋予函数,则直接调用; 否则首先调用老的
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function () {
+      oldOnload();
+      func();
+    };
+  }
+}
+
+
+// 此处不能写成 "positionMessage()", 否则加载不出效果
+addLoadEvent(prepareSlideshow)
+
---
+++ chp10/10-2/scripts/prepareSlideshow.js
@@ -0,0 +1,35 @@
+function prepareSlideshow() {
+  // 确保浏览器智齿DOM方法
+  if (!document.getElementsByTagName) {
+    return false;
+  }
+  if (!document.getElementById) {
+    return false;
+  }
+
+  // 确保元素存在
+  if (!document.getElementById("linklist")) {
+    return false;
+  }
+  if (!document.getElementById("preview")) {
+    return false;
+  }
+  // 为图片应用样式
+  var preview = document.getElementById("preview");
+  preview.style.position = "absolute";
+  preview.style.left = "0px";
+  preview.style.top = "0px";
+  // 取得列表中的所有链接
+  var list = document.getElementById("linklist");
+  var links = list.getElementsByTagName("a"); /*取得所有a标签*/
+  // 为mouseover事件添加动画效果
+  links[0].onmouseover = function () {
+    moveElement("preview", -100, 0, 5);
+  };
+  links[1].onmouseover = function () {
+    moveElement("preview", -200, 0, 5);
+  };
+  links[2].onmouseover = function () {
+    moveElement("preview", -300, 0, 5);
+  };
+}
