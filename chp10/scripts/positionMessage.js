b9e9b18564b306f9448b347ee1a16737eadc4a32
zy <1475705241@qq.com>
2021-04-07 11:36:46 +0800

    两个元素同时移动

--- chp10/scripts/positionMessage.js
+++ chp10/scripts/positionMessage.js
@@ -11,6 +11,16 @@ function positionMessage() {
   elem.style.left = "50px";
   elem.style.top = "100px";
   moveElement("message", 200, 300, 10);
+
+  if (!document.getElementById("message2")) {
+    return false;
+  }
+
+  var elem2 = document.getElementById("message2");
+  elem2.style.position = "absolute"
+  elem2.style.left = "200px";
+  elem2.style.top = "30px";
+  moveElement("message2", 400, 50, 10);
 }
 
 
